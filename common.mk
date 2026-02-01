00012cfed334cfa72f10d24c1683111b4816739d
Oscar <osm1019@gmail.com>
2026-01-31 23:44:59 -0800

    sm8750-common: Build Device Settings
    Change-Id: I91d5570f6bde8bcb1dde133634ee49ddc74d9a9e

--- common.mk
+++ common.mk
@@ -33,6 +33,13 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     xbl_config_arb_check
 
+# Alert slider
+ifneq ($(TARGET_IS_TABLET),true)
+PRODUCT_PACKAGES += \
+    DeviceSettings \
+    tri-state-key-calibrate
+endif
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
